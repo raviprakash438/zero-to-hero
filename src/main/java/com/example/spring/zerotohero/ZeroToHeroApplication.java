531d0465db7e681de62fa798f80e8a871ea6c635
raviprakash438 <Happy@2209>
2021-05-30 14:18:23 +0530

    first commit

---
+++ src/main/java/com/example/spring/zerotohero/ZeroToHeroApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.zerotohero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroToHeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeroToHeroApplication.class, args);
+	}
+
+}
